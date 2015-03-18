71e3a0f78bedf6a0a130ea80b6eecdfca336df06
Mohit Jain <mandrake_jain21@yahoo.co.in>
2015-03-18 14:52:20 +0530

    Update ui.R

--- Developing_Data_Products/ShinyIRIS/ui.R
+++ Developing_Data_Products/ShinyIRIS/ui.R
@@ -9,6 +9,18 @@ shinyUI(fluidPage(
         # Sidebar with controls to select Summary or Plot for the data.
         sidebarLayout(
                 sidebarPanel(
+                        #Documentation
+                        p("Iris Flower Data is multivariate data set introduced by Sir Ronald Fisher (1936)
+                          as an example of discriminant analysis."),
+                        
+                        p("This simple app works a starting point to explore more about the data set. 
+                          User needs to select the desired plot type through the reactive drop down menu.
+                          This will display various plots for the dataset."),
+                        
+                        strong("In case of any issue, Please refresh the page."),
+                        
+                        p(""),
+                        
                         selectInput("Plot", "Choose a Plot Type:",
                                     c(
                                             "Summary" = "Summary",    
